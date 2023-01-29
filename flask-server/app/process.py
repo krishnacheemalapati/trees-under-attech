4a3187b207b6521300928f55dde0c2777d212e73
Rod2003 <rodrigo.daguila@gmail.com>
2023-01-29 02:53:40 -0500

    More data

--- flask-server/app/process.py
+++ flask-server/app/process.py
@@ -9,10 +9,8 @@ from keras.layers import Input, Dropout, Dense, Flatten, BatchNormalization, Con
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
-from keras.optimizers import Adam, SGD
+# from keras.optimizers import Adam, SGD
 from PIL import Image
-from sklearn.metrics import fbeta_score
-from sklearn.model_selection import train_test_split
 import cv2
 import os
 from tqdm import tqdm
@@ -21,3 +19,198 @@ path = "static/"
 train_path = os.path.join(path, "train-jpg")
 test_path = os.path.join(path, "test-jpg")
 class_path = os.path.join(path, "train_classes.csv")
+submission_path = os.path.join(path, "sample_submission.csv")
+
+#reading the labels
+df = pd.read_csv(class_path)
+submission_df = pd.read_csv(submission_path)
+print(df.shape)
+print(submission_df.shape)
+
+label_list = []
+for tag_split in df.tags.to_numpy():
+    labels = tag_split.split(' ')
+    for label in labels:
+        if label not in label_list:
+            label_list.append(label)
+labels_dict = dict(zip(range(0,17), label_list))
+
+# One-hot encode the features
+for label in label_list:
+    df[label] = df['tags'].apply(lambda x: 1 if label in x.split() else 0)
+    
+df.head(3)
+
+# plot the labels      
+df[label_list].sum().sort_values(ascending=False)\
+.plot(kind='barh', title='Label instance distribution', orientation= 'vertical', figsize=(10, 5), color='blue')
+plt.show()
+
+# for the analysis we need columns after tag and image_name
+df_columns = list(df.columns[2:])
+print(df_columns,end='')
+
+# Onehot encode the image name
+df['image_name'] = df['image_name'].apply(lambda x: f'{x}.jpg')
+df.head(3)
+
+def f_beta_score(y_true, y_pred, beta = 2, epsilon = 1e-4):
+    
+    beta_squared = beta**2
+    
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(tf.greater(tf.cast(y_pred, tf.float32), tf.constant(0.5)), tf.float32)
+    
+    tp1 = tf.reduce_sum(y_true, axis = 1)
+    tp2 = tf.reduce_sum(y_pred, axis = 1)
+    tp = tp1 * tp2
+    fp = tf.reduce_sum(y_pred, axis = 1) - tp
+    fn = tf.reduce_sum(y_true, axis = 1) - tp
+    
+    precision = tp/(tp +fp +epsilon)
+    recall = tp/(tp + fn + epsilon)
+    
+    return (1 + beta_squared) * precision * recall / (beta_squared * precision + recall + epsilon)
+
+def accuracy_score(y_true, y_pred, epsilon = 1e-4):
+    
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(tf.greater(tf.cast(y_pred, tf.float32), tf.constant(0.5)), tf.float32)
+    
+    tp1 = tf.reduce_sum(y_true, axis = 1)
+    tp2 = tf.reduce_sum(y_pred, axis = 1)
+    tp = tp1 * tp2
+    fp = tf.reduce_sum(y_pred, axis = 1) - tp
+    fn = tf.reduce_sum(y_true, axis = 1) - tp
+    
+    y_true = tf.cast(y_true, tf.bool)
+    y_pred = tf.cast(y_pred, tf.bool)
+        
+    tn1 = tf.reduce_sum(tf.cast(tf.logical_not(y_true), tf.float32), axis = 1) 
+    tn2 = tf.reduce_sum(tf.cast(tf.logical_not(y_pred), tf.float32), axis = 1)
+    tn = tn1 * tn2
+    return (tp + tn)/(tp + tn + fp + fn + epsilon)
+
+##Split training data to training and validation sets
+train_datagen = ImageDataGenerator(rescale = 1./255, validation_split = 0.2)
+
+# train_set contains 80 percent of images
+train_gen = train_datagen.flow_from_dataframe(dataframe=df,
+                                              directory = train_path,  
+                                              x_col="image_name", 
+                                              y_col=str(df_columns), 
+                                              subset="training", 
+                                              batch_size=128,
+                                              seed=42, 
+                                              shuffle = True, 
+                                              class_mode="raw", 
+                                              target_size=(128,128))
+
+
+# val_set contains 20 percent of images 
+val_gen = train_datagen.flow_from_dataframe(dataframe=df,
+                                            directory = train_path,  
+                                            x_col="image_name", 
+                                            y_col=str(df_columns), 
+                                            subset="validation", 
+                                            batch_size=128,
+                                            seed=42, 
+                                            shuffle = True, 
+                                            class_mode="raw", 
+                                            target_size=(128,128))
+
+##Building a sequential feed forward network in keras
+def model_building():
+    model = Sequential()
+    model.add(BatchNormalization(input_shape=(128, 128, 3)))
+    model.add(Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(17, activation='sigmoid'))
+    
+    return model
+
+model = model_building()
+model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=accuracy_score)
+
+model.save("my_model")
+
+# Save the callback
+callback = EarlyStopping(
+    monitor="val_loss", 
+    patience=2,
+    verbose=0,
+    baseline=None,
+    restore_best_weights=True,
+)
+
+# Batchsize for training and validation set
+train_steps = train_gen.n // train_gen.batch_size
+val_steps = val_gen.n // train_gen.batch_size
+
+history = model.fit(x=train_gen,
+                    steps_per_epoch=train_steps,
+                    validation_data=val_gen,
+                    validation_steps=val_steps,
+                    epochs=5,
+                    callbacks=[callback])
+
+# Test data: divide the submission dataframe into two splits as the image names after 40669 is different 
+test_df = submission_df.iloc[:40669]['image_name'].reset_index().drop('index', axis =1)
+
+test_df['image_name'] = submission_df['image_name'].apply(lambda x: '{}.jpg'.format(x))
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+test_gen = test_datagen.flow_from_dataframe(dataframe=test_df,
+                                            directory = test_path,  
+                                            x_col="image_name", 
+                                            y_col="", 
+                                            batch_size=67,
+                                            shuffle=False,
+                                            class_mode="", 
+                                            target_size=(128,128))
+
+test_size = test_gen.n // test_gen.batch_size
+
+pred = model.predict(test_gen, steps=test_size, verbose=1)
+
+test_names = test_gen.filenames 
+        
+test_result = pd.DataFrame(pred)
+test_result = test_result.apply(lambda x: ' '.join(np.array(label_list)[x >= 0.2]), axis=1)
+
+test_result_df = pd.DataFrame({'image_name': test_names, 'tags': test_result})
+test_result_df.head()
+
+final_result = test_result_df.reset_index().drop('index', axis =1)
+
+# Remove the .jpg extension from the image_name of the final_result
+final_result['image_name'] = final_result['image_name'].apply(lambda x: x[:-4])
+
+# save the results to a CSV file save and set the index to false.
+final_result.to_csv('submission.csv', index=False)
