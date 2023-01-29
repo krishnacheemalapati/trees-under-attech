acbd10daf20a087b47860c42ab50ca9f8b770542
Rod2003 <rodrigo.daguila@gmail.com>
2023-01-28 22:44:32 -0500

    Edit flask init

---
+++ flask-server/app/process.py
@@ -0,0 +1,12 @@
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImageClassification()
+prediction.setModelTypeAsDenseNet121()
+prediction.setModelPath(os.path.join(execution_path, "static/densenet121-a639ec97.pth"))
+prediction.loadModel()
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "static/horse.jpg"), result_count=10)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , ":" , eachProbability)
